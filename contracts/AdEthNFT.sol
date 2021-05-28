05090a9d8452f3287019b19e3ab321fd9eb93b98
raphaelpg <raphael_pinto@outlook.com>
2021-05-29 00:53:29 +0100

    start coding AdEthNFT.sol

--- contracts/AdEthNFT.sol
+++ contracts/AdEthNFT.sol
@@ -3,6 +3,28 @@ pragma solidity ^0.8.4;
 
 contract AdEthNFT {
   //variables
+  address public owner;
+  address public factory;
+  address public adCaller;
+  string public uri;
+  uint8 public cpc;
+  
+  mapping(address => bool) public whitelist;
+  
+  //events
+
   //constructor
-  //function
+  constructor(address _newOwner, address _newAdCaller, string memory _newUri, uint8 _newCpc) {
+    factory = msg.sender;
+    owner = _newOwner;
+    adCaller = _newAdCaller;
+    uri = _newUri;
+    cpc = _newCpc;
+  }
+
+  //functions
+  //function beenClicked()
+  //function whitelistAddress()
+  //function blacklistAddress()
+  //function callerBalance()
 }
