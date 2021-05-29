af184b25b4d7482e2933382b77583d127eab799c
raphaelpg <raphael_pinto@outlook.com>
2021-05-29 13:11:54 +0100

    start AdEth front app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import AdEthFactoryContract from "./contracts/AdEthFactory.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { feeValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,9 +17,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = AdEthFactoryContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        AdEthFactoryContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -39,13 +39,13 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.methods.setFee(10).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
+    const currentFee = await contract.methods.fee().call();
+    console.log(currentFee)
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ feeValue: currentFee });
   };
 
   render() {
@@ -64,7 +64,7 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The fee value is: {this.state.feeValue}</div>
       </div>
     );
   }
