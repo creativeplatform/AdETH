bf42dbf51f60c5adad4b9514dddd97bb8741ace3
raphaelpg <raphael_pinto@outlook.com>
2021-05-30 18:37:44 +0100

    update readme file

--- mockWebsite/src/config/config.js
+++ mockWebsite/src/config/config.js
@@ -2,8 +2,8 @@ const WEB3 = {
 	erc20Name: "dai",
 	erc20Address: "0x05C64669dD8fb5d0264DDa8C5125eA339dAED657",
 	adEthFactoryAddress: "0x355c48b65Bca9bfc8f3f13F2d512E1678d19470C",
-	adCallerPrivateKey: "0x98cd7db0c4d4853ae6fdd16ee4ec79987edf104969af41993ea5c0f3742aa30d",
-  adEthNFTAddress: "0x15A7D14859548c3740eD583d4118f453C9752A74",
+	adCallerPrivateKey: "0x8f9bd846b4a85e20b2da19fbce9bc987ed5095b71bbd3781404b529d210e6682",
+  adEthNFTAddress: "0xdfAafd076f3105a6abe8E6451014b85af84ceB52",
 	websiteAddress: "0x95A228696747c6e837D00a6963BacD1b0Ad00fCa",
 };
 
